6f8cf3cdf2b5924cff975a991dddbbb8e21efd0e
Waliko <kalbit@bk.ru>
2024-12-24 16:15:25 +0300

    fix: correcting return type of router

--- router.py
+++ router.py
@@ -16,7 +16,7 @@ async def add_task(
         task: Annotated[STaskAdd, Depends()]
 ) -> STaskId:
     task_id = await TaskRepository.add_one(task)
-    return {'ok': True, 'task_id': task_id}
+    return STaskId(ok=True, task_id=task_id)
 
 
 @router.get('')
